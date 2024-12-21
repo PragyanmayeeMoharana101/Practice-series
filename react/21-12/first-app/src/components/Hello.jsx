86bae3a9f66ea8cf732871bc4587cf1dd716bee6
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2024-12-21 17:12:03 +0530

    creating and acessing array in component

--- react/21-12/first-app/src/components/Hello.jsx
+++ react/21-12/first-app/src/components/Hello.jsx
@@ -1,13 +1,19 @@
-// creating and acessing variable in component
+// creating and acessing array in component
 const name = "ram";
 const age = 20;
-const height = 5.4
+const height = 5.4;
+const hobbies = ["reading books", "painting"]
 function Hello (){
     return (
         <>
         <h1>Hello my friend {name}</h1>
         <h1>you are {age} years old</h1>
         <h1>you are {height} feet tall</h1>
+        <h1>your hobbies are - 
+            {hobbies.map ((hobby)=>{
+                return <span> "{hobby}" ,</span>
+            })}
+        </h1>
         </>
     ) 
 }
