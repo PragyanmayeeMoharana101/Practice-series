adfab90c4406568dcb9a66d97ec3e9de751224fb
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2024-12-21 17:29:30 +0530

    creating and acessing object in component

--- react/21-12/first-app/src/components/Hello.jsx
+++ react/21-12/first-app/src/components/Hello.jsx
@@ -1,24 +1,15 @@
-//creating local variables instead of global
+//creating and acessing object in component
 function Hello (){
-    const name = "ram";
-    const age = 20;
-    const height = 5.4;
-    const hobbies = ["reading books", "painting"]
-    function message(){
-        return "good luck for your react career"
+    const student ={
+        name : "ram",
+        age : 20,
+        height: 5.4
     }
     return (
         <>
-        <h1>Hello my friend {name}</h1>
-        <h1>you are {age} years old</h1>
-        <h1>you are {height} feet tall</h1>
-        <h1>your hobbies are - 
-            {hobbies.map ((hobby)=>{
-                return <span> "{hobby}" ,</span>
-            })}
-        </h1>
-        <h2>{message()}</h2>
-        <h2>total rating = {20+10}</h2>
+        <h1>Hello my friend {student.name}</h1>
+        <h1>you are {student.age} years old</h1>
+        <h1>you are {student.height} feet tall</h1>
         </>
     ) 
 }
