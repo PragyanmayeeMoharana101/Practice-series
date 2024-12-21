466328b9b7c70a73c8326b88f9dffc5fefb3cd6b
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2024-12-21 16:20:27 +0530

    displaying hello world using diffrent component

--- react/21-12/first-app/src/App.jsx
+++ react/21-12/first-app/src/App.jsx
@@ -1,6 +1,8 @@
-//displaying hello world
+//displaying hello world using diffrent component
 function App() {
+  return <Hello/>
+}
+function Hello (){
   return <h1>Hello World</h1>
 }
-
 export default App
