451d32795e5758c477225f12c46d1c0f96cb11b1
pragyanmayeem <pragyanmayeemoharana101@gmail.com.>
2024-12-21 16:24:08 +0530

    wrapping multiple cpmponents using div tag

--- react/21-12/first-app/src/App.jsx
+++ react/21-12/first-app/src/App.jsx
@@ -1,8 +1,16 @@
-//displaying hello world using diffrent component
+//wrapping multiple components using div tag
 function App() {
-  return <Hello/>
+  return (
+    <div>
+      <Hello/>
+      <WelcomeMsg/>
+    </div>
+  )
 }
 function Hello (){
   return <h1>Hello World</h1>
+}
+function WelcomeMsg(){
+  return <h1>Welcome to React</h1>
 }
 export default App
